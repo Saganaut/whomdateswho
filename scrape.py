e6f86a3429082731a059130499d27731316b96e4
Yacin Nadji <yacin@gatech.edu>
2015-09-03 17:34:24 -0400

    parse main celeb page

--- scrape.py
+++ scrape.py
@@ -10,6 +10,14 @@ from bs4 import BeautifulSoup
 
 mainurl = 'http://dating.famousfix.com/?tab=&pageno=%d'
 
+def getcelebs(pageindex):
+    soup = BeautifulSoup(urllib2.urlopen(mainurl % pageindex), 'html.parser')
+    for celebli in soup.find(id='container').find_all('li'):
+        name = celebli.a['title']
+        imgurl = celebli.a.img['src']
+        profileurl = celebli.div.a['href']
+        yield name, imgurl, profileurl
+
 def main():
     """main function for standalone usage"""
     usage = "usage: %prog [options]"
